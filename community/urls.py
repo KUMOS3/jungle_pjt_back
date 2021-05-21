a8f5a3a5aa471a8dd9553eeda93b8dae92af1499
Ryan <cho8213456@gmail.com>
2021-05-21 15:17:08 +0900

    review create delete comment create

--- community/urls.py
+++ community/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('reviews/', views.review_list),
-    # path('reviews/<int:review_pk>/', views.review_detail), # 추가
+    path('reviews/<int:review_pk>/delete/', views.review_delete),
+    path('reviews/<int:review_pk>/comments/', views.comment_create),
     path('<int:review_pk>/likes/', views.likes),
 ]
