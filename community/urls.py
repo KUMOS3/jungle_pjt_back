771569d7f07f2a02e11b4a75920abfef957e1367
Ryan <cho8213456@gmail.com>
2021-05-22 16:41:40 +0900

    delete

--- community/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reviews/', views.review_list),
-    path('reviews/<int:review_pk>/delete/', views.review_delete),
-    path('reviews/<int:review_pk>/comments/', views.comment_create),
-    path('<int:review_pk>/likes/', views.likes),
-]
--- movies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('movies/', views.movie_list), # 이제 앱 네임 필요없음
-    
-]
